ca505dce4a29baed259e3a0abc8aad3cf3111931
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-10-21 21:00:26 +0300

    feat: database connected

---
+++ db/db.client.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { MONGO_CONNECTION_STRING } = require('../src/common/config');
+
+const connectToDB = cb => {
+  mongoose.connect(MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', async () => {
+    console.log('DB connected!');
+    await db.dropDatabase();
+    cb();
+  });
+};
+
+module.exports = { connectToDB };
