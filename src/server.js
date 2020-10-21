72519b6b7a555ce175e61040d82c2f7021cb5a28
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-10-21 21:20:09 +0300

    feat: add connectToDB in server.js

--- src/server.js
+++ src/server.js
@@ -11,8 +11,11 @@ process
   });
 
 const { PORT } = require('./common/config');
+const { connectToDB } = require('../db/db.client');
 const app = require('./app');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+connectToDB(() => {
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+});
